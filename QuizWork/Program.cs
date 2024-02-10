33add0f1595b18869974cfac8a26402f294fb166
visual studio <vs@itstep.ge>
2024-02-10 19:46:06 +0400

    r

--- QuizWork/Program.cs
+++ QuizWork/Program.cs
@@ -1,123 +1,14 @@
 ﻿using System.Collections.Generic;
 
-class Program
+namespace QuizWork
 {
-    static void Main(string[] args)
+    class Program
     {
-        string[] quiz = new string[]
+        static void Main(string[] args)
         {
-            "Capital of France?:Paris",
-            "Capital of Georgia?:Tbilisi",
-            "Where is Berlin located?:Germany",
-            "Where is China located?:Asia",
-            "Capital of Italy?:Rome",
+            Functionals fn = new Functionals();
 
-            "Capital of saudi arabia?:Riyadh",
-            "Capital of UAE:Abhu dhabi",
-            "Capital of Malaysia?:Kuala Lumpur",
-            "Where is Egypt located?:Africa",
-            "Capital of Mexico?:Mexico city",
-
-            "Where is Canada located?:North America",
-            "Capital of United states?:Washington DC",
-            "Capital of UK?:London",
-            "Where is Poland located?:Europe",
-            "Capital of Russia?:Moscow",
-
-            "Capital of Japan?:Tokyo",
-            "Capital of China?:Beijing",
-            "Where is Vienna located?:Austria",
-            "Where is Canberra located?:Australia",
-            "Capital of Portugal?:Lisbon",
-
-            "What do people usually eat on Thanskgiving?:Turkey",
-            "Who painted the Starry Night?:Vincent van Gogh",
-            "Who carved the Statue of David?:Michelangelo",
-            "Capital of Papua New Guinea?:Port Moresby",
-            "Longest River in China?:Yangtze River",
-
-            "Largest lake in the World?:Caspian Sea",
-            "Capital of Netherlands?:Amsterdam",
-            "What does OOP Mean?:Object Oriented Programming",
-            "What does CSS Mean?:Cascading Style Sheets",
-            "Who Painted The Last Supper?:Leonardo da Vinci"
-        };
-
-        //Random rnd = new Random();
-        //List<int> indexes = new List<int>() ;
-        //int index = 0;
-        //for (int i = 0; i < 5; i++)
-        //{
-        //    index = rnd.Next(0, quiz.Length );
-
-        //    if (!indexes.Contains(index))
-        //    {
-        //        var gg = quiz[index].Split(":");
-        //        Console.WriteLine(gg[0]);
-        //    }
-        //    Console.ReadLine();
-        //}
-
-        using (StreamWriter qwrite = new StreamWriter("test.txt"))
-        {
-            foreach (string question in quiz)
-            {
-                qwrite.WriteLine(question);
-            }
+            fn.Funcs();
         }
-
-        string[] thinredline = File.ReadAllLines("test.txt");
-
-        int points = 0;
-
-      
-            //var ss = line.Split(":");
-            //Console.WriteLine(ss[0]);
-
-
-            Random rnd = new Random();
-            List<int> indexes = new List<int>();
-            int index = 0;
-
-            for (int i = 0; i < 5; i++)
-            {
-
-                index = rnd.Next(0, quiz.Length);
-                var gg = quiz[i].Split(":");
-
-
-                Console.WriteLine(gg[0]);
-
-
-
-                var answer = Console.ReadLine();
-
-                if (answer.ToLower() == gg[1].ToLower())
-                {
-                    Console.WriteLine("Correct");
-                    ++points;
-                }
-                if (answer != gg[1].ToLower())
-                {
-                    Console.WriteLine($"Incorrect, answer was {gg[1]}");
-                }
-
-
-            }
-
-
-
-            if (points == 5)
-            {
-                Console.WriteLine("Congratulations, you got all 5 points!");
-            }
-            if (points == 0)
-            {
-                Console.WriteLine("You got 0 points, try again?");
-            }
-            else
-            {
-                Console.WriteLine($"Congratulations, you got {points} out of 5 points");
-            }
         }
     }
